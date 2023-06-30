b0241645cca98e1f4d22a064eaa6f58e69ec504f
User <User@LAPTOP-QBAUTE79>
2023-06-30 20:20:39 +0200

    Dodaj pliki projektów.

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿namespace Zadanie1_programowanie2023
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Osoba osoba = new Osoba("Antonina Kowal");
+            Osoba osoba2 = new Osoba("Eleonora Byk");
+            osoba.DataUrodzenia = new DateTime(1998, 02, 11);
+            osoba.DataŚmierci = new DateTime(2010, 07, 20);
+
+
+            Console.WriteLine(osoba.Imię);
+            Console.WriteLine(osoba.Nazwisko);
+            Console.WriteLine(osoba.DataUrodzenia?.ToString("yyyy-MM-dd"));
+            Console.WriteLine(osoba.DataŚmierci?.ToString("yyyy-MM-dd"));
+            Console.WriteLine(osoba.Wiek?.TotalDays);
+            Console.WriteLine(osoba.ImięNazwisko);
+        }
+    }
+
+}
---
+++ Osoba.cs
@@ -0,0 +1,88 @@
+﻿public class Osoba
+
+{
+    private string imięNazwisko;
+
+    public string ImięNazwisko
+    {
+        get { return imięNazwisko; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Imię nie może być puste.");
+            }
+
+            imięNazwisko = value;
+            AktualizujImięINazwisko(value);
+        }
+    }
+
+    public Osoba(string imięNazwisko)
+    {
+        ImięNazwisko = imięNazwisko;
+    }
+
+    private string? imię;
+    public string Imię
+    {
+        get => imię;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Imię nie może być puste.");
+            }
+            imię = value;
+        }
+    }
+
+    private string? nazwisko;
+    public string Nazwisko
+    {
+        get => nazwisko;
+        set { nazwisko = value; }
+    }
+
+    public DateTime? DataUrodzenia { get; set; }
+    public DateTime? DataŚmierci { get; set; }
+
+    public TimeSpan? Wiek
+    {
+        get
+        {
+            if (DataUrodzenia == null)
+            {
+                return null;
+            }
+
+            DateTime śmierc_czyZyjeDoDzisiaj = DataŚmierci ?? DateTime.Now.Date;
+            return śmierc_czyZyjeDoDzisiaj - DataUrodzenia.Value;
+        }
+    }
+
+    private void AktualizujImięINazwisko(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            imię = null;
+            nazwisko = null;
+        }
+        else
+        {
+            string[] split = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (split.Length > 0)
+            {
+                imię = split[0];
+                if (split.Length > 1)
+                {
+                    nazwisko = split[split.Length - 1];
+                }
+                else
+                {
+                    nazwisko = string.Empty;
+                }
+            }
+        }
+    }
+}
